f1817ccbc0a98c5562290bb15b7a295031c777ea
Philippe ROBERT <probert@e1r11p4.42.fr>
2017-09-21 16:27:12 +0200

    Commandes Shell

---
+++ Commandes_Shell.sh
@@ -0,0 +1,4 @@
+alias ppoub="find . -type f \( -name \"*.out\" -o -name \"*.swp\" -o -name \".DS_*\" \) -print "
+alias dpoub="find . -type f \( -name \"*.out\" -o -name \"*.swp\" -o -name \".DS_*\" \) -delete"
+alias gc="gcc -Wall -Werror -Wextra"
+alias norme="norminette -R CheckForbiddenSourceHeader"
